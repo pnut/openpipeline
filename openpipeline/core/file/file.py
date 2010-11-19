b71cfa806d1fc40210c02a79a3c424f59db238dd
jungum1 <jungum1@2606bc13-7c89-da30-fe90-c849d5d71086>
2010-11-19 20:41:13 +0000

    by Jung on the first day

--- openpipeline/core/file/file.py
+++ openpipeline/core/file/file.py
@@ -1,9 +1,10 @@
 import os
 
-class File:
-    def __init__(self):
-        pass
-  
+
+class fileCore ():
+    def __init__(self, debug=0):
+        self.debug = debug
+        
     def query(self, path):
         if os.path.exists(path):
             return 1
@@ -11,5 +12,14 @@ class File:
             return 0
 
     def bundle(self):
-        pass        
-   
+        pass
+    
+    def fileCoreFileName(self):
+        fileName = os.path.split(self.filePath)
+        if self.debug: print 'the fileName : ' + fileName[1]
+
+    def fileMayaLoc (self):
+        mayaLoc = os.path.split(self.filePath)
+        locDir = mayaLoc[0]
+        if self.debug: print 'the locDir : ' + locDir
+        return locDir
