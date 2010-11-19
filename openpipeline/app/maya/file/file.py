0905637f6ce65349e71cd4c7f4234a0fbe7106f2
pyapor <pyapor@2606bc13-7c89-da30-fe90-c849d5d71086>
2010-11-19 20:52:25 +0000

    fileMAya

--- openpipeline/app/maya/file/file.py
+++ openpipeline/app/maya/file/file.py
@@ -10,5 +10,46 @@ class fileMaya (file.File):
         currentPath = cmds.file(q = True, exn = True)
         if self.debug: print 'currentPath : ' + currentPath
     
-
+    #Returns the name of the currently opened file. ##pyapor##
+    def mayaFileName(self):
+        filePath = self.myFilePath()
+        Path = os.path.split(filePath)
+        fileName = Path[1]
+        return fileName
+        if self.debug: print fileName
+        
+    #Returns folder where currently opened file lives. ##pyapor## 
+    def mayaLoc(self):
+        loc = self.myFilePath()
+        if self.debug: print loc
+        locPath = os.path.split(loc)
+        locDir = locPath[0]
+        if self.debug: print locDir
+        return locDir
+    
+    def mayaSave(self):
+        fileSave = cmds.file(s = True)
+        if self.debug: print fileSave + ' has been saved.'
+    
+    #What it does is pretty much rename and save. ##pyapor##
+    def mayaSaveAs(self, folderPath, name):
+        if self.debug: print name
+        if self.debug: print folderPath
+        renamePath = os.path.join(Path, name)
+        if self.debug: print renamePath 
+        
+        if self.query(renamePath):
+            if self.debug: print 'File name already existis'    
+        else:
+            cmds.file(rn = renamePath)
+            self.mySave()
+            if self.debug: print 'File has been renamed to ' + self.newName
+    
+    #Starts a new file but gives you the option to either save the current scene or not to do so. ##pyapor##
+    def mayaNew(self, bool=0):
+        if bool == 1:
+            self.mysave()
+            cmds.file(new = True, f = True)
+        else:
+            cmds.file(new = True, f = True)
     
