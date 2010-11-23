4e46b2a82eafae838805db988e0e07cc8f2dbc91
jungum1 <jungum1@2606bc13-7c89-da30-fe90-c849d5d71086>
2010-11-23 19:48:32 +0000

    by Jung

--- openpipeline/core/file/file.py
+++ openpipeline/core/file/file.py
@@ -14,12 +14,26 @@ class fileCore ():
     def bundle(self):
         pass
     
+    #get the file name.
     def fileCoreFileName(self):
         fileName = os.path.split(self.filePath)
         if self.debug: print 'the fileName : ' + fileName[1]
-
+        
+    #get the path without the file name.
     def fileMayaLoc (self):
         mayaLoc = os.path.split(self.filePath)
         locDir = mayaLoc[0]
         if self.debug: print 'the locDir : ' + locDir
         return locDir
+    
+    #make a folder.
+    def fileCoreMakeFolder(self, folderName):
+        folderPath = os.path.join(self.filePath, folderName)
+
+	if self.query(folderPath):
+            if self.debug: print 'the folder' + folderName + 'is already exist.'
+            pass
+	else:
+            os.mkdir(self.filePath + "/" + folderName)
+	    if self.debug: print 'the folder' + folderName + ' has been created under : ' + folderPath	    
+
